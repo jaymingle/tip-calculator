52e58a9a92ff08a91b26552a1e24695721ea0752
Jay Mingle <jkawudi@gmail.com>
2023-12-29 01:25:28 +0000

    Changes

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ let billValue = document.querySelector("#billAmount")
 // document.querySelector("#theTestButton").onclick = function(e){
 //     console.log(billValue.value)
 // }
+
+let formSubmit = document.querySelector("#theForm")
+formSubmit.addEventListener("submit", (e) => {
+    console.log("Submitted")
+})
