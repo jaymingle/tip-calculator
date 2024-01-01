1eafd4a3e8f3ea1a90173f283c1b0486d40bb41c
Jay Mingle <jkawudi@gmail.com>
2024-01-01 04:00:53 +0000

    Changes

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function calculateForm(){
 
     const bill = document.querySelector("#billAmount").value
     const percentageTip = document.querySelector("input[name='tip']:checked").value
-
+    const people = document.querySelector("#numberOfPeople").value
     console.log(bill)
     console.log(percentageTip)
+    console.log(people)
+
+
 }
