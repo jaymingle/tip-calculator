4151498b0a04a237d75663fccac63d4643e42293
Jay Mingle <jkawudi@gmail.com>
2024-01-01 04:03:49 +0000

    Changes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function calculateForm(){
     // console.log("Calculating")
 
-    const bill = document.querySelector("#billAmount").value
-    const percentageTip = document.querySelector("input[name='tip']:checked").value
-    const people = document.querySelector("#numberOfPeople").value
+    const bill = Number(document.querySelector("#billAmount").value)
+    const percentageTip = Number(document.querySelector("input[name='tip']:checked").value)
+    const people = Number(document.querySelector("#numberOfPeople").value)
     console.log(bill)
     console.log(percentageTip)
     console.log(people)
 
     const tipValue = Number(percentageTip) / 100 * Number(bill)
     console.log(tipValue)
+    const
 
 }
