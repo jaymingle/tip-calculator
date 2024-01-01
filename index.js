6034bdd30116fdef786922dcf9ca514f5ba3be43
Jay Mingle <jkawudi@gmail.com>
2024-01-01 03:59:44 +0000

    Changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function calculateForm(){
     // console.log("Calculating")
 
     const bill = document.querySelector("#billAmount").value
-    const percentageTip = document.querySelector("input[type='radio']").value
+    const percentageTip = document.querySelector("input[name='tip']:checked").value
 
     console.log(bill)
     console.log(percentageTip)
