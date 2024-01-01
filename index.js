684e676f9750d1ded642fea2901127c72f977658
Jay Mingle <jkawudi@gmail.com>
2024-01-01 04:33:29 +0000

    Changes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function calculateForm(){
     // console.log("Calculating")
 
+    if(!)
+
     const bill = Number(document.querySelector("#billAmount").value)
     const percentageTip = Number(document.querySelector("input[name='tip']:checked").value)
     const people = Number(document.querySelector("#numberOfPeople").value)
-    console.log(bill)
-    console.log(percentageTip)
-    console.log(people)
+    // console.log(bill)
+    // console.log(percentageTip)
+    // console.log(people)
 
     const tipValue = Number(percentageTip) / 100 * Number(bill)
     const totalExpenses = bill + tipValue
@@ -15,4 +17,16 @@ function calculateForm(){
     document.querySelector("#showTotalExpense").innerText += "GHS " + totalExpenses;
     document.querySelector("#tipValue").innerText += "GHS " + tipValue
     document.querySelector("#costPerPerson").innerText += "GHS " + individualExpenses
+}
+
+function resetFields(){
+    console.log("Reset")
+    document.querySelector("#billAmount").value = ''
+    document.querySelector("#numberOfPeople").value = ''
+    document.querySelector("#showTotalExpense").innerText = " ";
+    document.querySelector("#tipValue").innerText = " "
+    document.querySelector("#costPerPerson").innerText = " "
+
+    let radioButtons = document.querySelectorAll("input[name='tip']")
+    radioButtons.forEach(radio => radio.checked = false)
 }
